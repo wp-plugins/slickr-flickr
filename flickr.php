fbb0ac9cc84fefe612ade931b8492682ecf54acf
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-05 17:22:14 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/slickr-flickr/tags/1.1@213563 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ flickr.php
@@ -0,0 +1,49 @@
+<?php
+class flickr {
+  /* Function that removes double-quotes */
+  function cleanup($s = null) {
+    return $s?str_replace('"', '', $s):false;
+  }
+
+  /* Function that returns the correctly sized photo URL. */
+  function resize_photo($url, $size) {
+    
+    $url = explode('/', $url);
+    $photo = array_pop($url);
+
+    switch($size)  {
+      case 'square': $suffix = '_s.';  break;
+      case 'thumb': $suffix = '_t.';  break;
+      case 'small': $suffix = '_m.';  break;
+      case 'large': $suffix = '_b.';  break;
+      default:  $suffix = '.';  break; // Medium   
+      }
+
+    $url[] =  preg_replace('/(_(s|t|m|b))?\./i', $suffix, $photo);;
+    return implode('/', $url);
+  }
+
+  /* Function that get the photo url, size, height and original URL from feed item */
+  function find_photo($item) {
+    $photo['title'] = $item->get_title();
+
+    $data = $item->get_description();
+
+    preg_match_all('/<img src="([^"]*)"([^>]*)>/i', $data, $m);
+    $photo['url'] = $m[1][0];
+ 
+    preg_match_all('/width="([^"]*)"([^>]*)>/i', $data, $m);
+    $photo['width'] = $m[1][0];
+
+    preg_match_all('/height="([^"]*)"([^>]*)>/i', $data, $m);
+    $photo['height'] = $m[1][0];
+
+    $photo['orientation'] = $photo['height'] > $photo['width'] ? "portrait" : "landscape" ;
+
+    $enclosure = $item->get_enclosure(0);
+    $photo["original"] = $enclosure==null ? $photo['url'] : $enclosure->get_link();
+
+    return $photo;
+  }
+}
+?>
