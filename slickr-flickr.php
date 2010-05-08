5b4558c5a06aba2fcc9992a7bf18a6763982a5d3
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-08 14:34:46 +0000

    Tagging Slickr Flickr Version 1.4
    git-svn-id: https://plugins.svn.wordpress.org/slickr-flickr/tags/1.4@238690 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- slickr-flickr.php
+++ slickr-flickr.php
@@ -1,12 +1,14 @@
 <?php
 /*
 Plugin Name: Slickr Flickr
-Plugin URI: http://www.wordpresswise.com/slickr-flickr
+Plugin URI: http://slickr-flickr.diywebmastery.com
 Description: Displays tagged photos from Flickr in galleries and slideshows
-Version: 1.3
+Version: 1.4
 Author: Russell Jamieson
 Author URI: http://www.wordpresswise.com
 */
+
+$slickr_flickr_options = array();
 
 $slickr_flickr_defaults = array(
     'type' => 'gallery',
@@ -18,21 +20,34 @@ $slickr_flickr_defaults = array(
     'start' => '1',
     'group' => 'n',
     'id' => '',
-    'tag' => ''
+    'tag' => '',
+    'tagmode' => 'all',
+    'lightbox' => 'lightbox',
+    'descriptions' => 'off',
+    'flickr_link' => 'off',
+    'link' => '',
+    'attribution' => '',
+    'photos_per_row' => '',
+    'sort' => '',
+    'direction' => ''
     );
 
 function slickr_flickr_get_options () {
-global $slickr_flickr_defaults;
+   global $slickr_flickr_options;
+   global $slickr_flickr_defaults;
+   if ($slickr_flickr_options.length >= 1) return $slickr_flickr_options;
+
    $flickr_options = array();
    $options = get_option("slickr_flickr_options");
    if (empty($options)) {
-     return $slickr_flickr_defaults;
+      $slickr_flickr_options = $slickr_flickr_defaults;
    } else {
      foreach ($options as $key => $option) {
        if (isset($options[$key]) && strpos($key,"flickr_")==0)  $flickr_options[substr($key,7)] = $option;
      }
-     return shortcode_atts( $slickr_flickr_defaults, $flickr_options);
+     $slickr_flickr_options =  shortcode_atts( $slickr_flickr_defaults, $flickr_options);
    }
+   return $slickr_flickr_options;
 }
 
 if (is_admin()) {
