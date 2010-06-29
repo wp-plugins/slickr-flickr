20ade85406907ee11b8db222579302c606f92aa5
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-29 09:24:05 +0000

    Tagging version 1.10
    git-svn-id: https://plugins.svn.wordpress.org/slickr-flickr/tags/1.10@258467 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- slickr-flickr.js
+++ slickr-flickr.js
@@ -60,12 +60,14 @@ jQuery.noConflict(); jQuery(document).ready(function($) {
         else
             $(this).galleria();
     });
-    $(".slickr-flickr-gallery").each( function (index) {
-        $delay = $(this).data("delay");
-        if (($delay) && ($delay > 0))
-            $(this).find('a[rel="sf-lbox-auto"]').lightBox( { nextSlideDelay : 1000 * $delay });
-        else
-            $(this).find('a[rel="sf-lbox-manual"]').lightBox();
-  });
+    if ($('a[rel="sf-lbox-auto"]').size() + $('a[rel="sf-lbox-manual"]').size() > 0) {
+        $(".slickr-flickr-gallery").each( function (index) {
+            $delay = $(this).data("delay");
+            if (($delay) && ($delay > 0))
+                $(this).find('a[rel="sf-lbox-auto"]').lightBox( { nextSlideDelay : 1000 * $delay });
+            else
+                $(this).find('a[rel="sf-lbox-manual"]').lightBox();
+        });
+    }
   slickr_flickr_start_slideshows();
 });
